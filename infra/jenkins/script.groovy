2aac646ed73a6142280bdc24dda777bf1de1f05c
karosi12 <adekayor@gmail.com>
2024-03-26 20:42:50 +0200

    ch: docker-compose up

--- infra/jenkins/script.groovy
+++ infra/jenkins/script.groovy
@@ -2,7 +2,8 @@ def deployApp(){
     sh "ls -la"
 
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 cd aguadulce && git pull origin main && docker-compose up -d"
+        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 cd aguadulce && git pull origin main"
+        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 cd aguadulce && docker-compose up -d"
     }     
     echo "success - Deployed"
 }
