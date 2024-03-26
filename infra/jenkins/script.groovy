5a53a2e5daf33c43bbac9cb36b5456287bd536a3
karosi12 <adekayor@gmail.com>
2024-03-26 20:22:54 +0200

    feat: command process [wip]

--- infra/jenkins/script.groovy
+++ infra/jenkins/script.groovy
@@ -2,7 +2,7 @@ def deployApp(){
     sh "ls -la"
 
     sshagent(['ec2-server-key']) {
-        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 ls -la"
+        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 cd aguadulce && git pull origin https://github.com/karosi12/aguadulce.git && docker-compose up -d"
     }     
     echo "success - Deployed"
 }
