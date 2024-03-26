7fb8746383d420ba3bc32ab9293e71150b5eb926
karosi12 <adekayor@gmail.com>
2024-03-26 14:38:54 +0200

    ch: update groovy script

--- infra/jenkins/script.groovy
+++ infra/jenkins/script.groovy
@@ -1,5 +1,20 @@
+def deployApp(){
+    sh "ls -la"
+    def ec2Instance = "ubuntu@${env.IP_ADDRESS}"
+
+    sshagent(['ec2-server-key']) {
+        sh "scp -o StrictHostKeyChecking=no docker-compose.yaml ${ec2Instance}:/home/ubuntu"
+        sh "ssh -o StrictHostKeyChecking=no ${ec2Instance}"
+    }     
+    echo "success - Deployed"
+}
+
 def runTest() {
-    sh "echo 'Hello World'"
+    dir("src") {
+        //  install all dependencies needed for running tests
+        sh "npm install"
+        sh "npm run test"
+    } 
 }
 
 return this
