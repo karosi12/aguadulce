928adcb87980dd8e72db72d550c8f60af2da2bb1
karosi12 <adekayor@gmail.com>
2024-03-26 21:44:41 +0200

    ch: stop and start container

--- infra/jenkins/script.groovy
+++ infra/jenkins/script.groovy
@@ -3,7 +3,7 @@ def deployApp(){
 
     sshagent(['ec2-server-key']) {
         sh '''
-          ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 'cd aguadulce && git pull origin main && npm install && docker-compose up --build -d'
+          ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 'cd aguadulce && git pull origin main && npm install && docker-compose stop backend && docker-compose up --build -d'
         '''
     }     
     echo "success - Deployed"
