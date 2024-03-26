1ad3e55ed8438e16a79c792597452e26a70ee687
karosi12 <adekayor@gmail.com>
2024-03-26 20:16:00 +0200

    ch: check if ssh is successful

--- infra/jenkins/script.groovy
+++ infra/jenkins/script.groovy
@@ -2,9 +2,7 @@ def deployApp(){
     sh "ls -la"
 
     sshagent(['ec2-server-key']) {
-        sh '''
-            ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 'git pull origin https://github.com/karosi12/aguadulce.git && cd aguadulce && docker-compose up -d'
-        '''
+        sh "ssh -o StrictHostKeyChecking=no ubuntu@3.122.244.24 ls -la"
     }     
     echo "success - Deployed"
 }
